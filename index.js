14d2163688d306bcee1e10124cc6b62eed2caed1
Michael Christopher <funnyjokesters@gmail.com>
2024-05-18 16:44:26 -0400

    Change_email_subscription

--- index.js
+++ index.js
@@ -4,6 +4,38 @@ const navLink = document.querySelectorAll(".nav-link");
 
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
+// Function to set a cookie
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Function to get a cookie
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+// Function to erase a cookie
+function eraseCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
+}
+
+// Example usage
+setCookie('username', 'JohnDoe', 7); // Sets a cookie 'username' with value 'JohnDoe' for 7 days
+var username = getCookie('username'); // Gets the value of the 'username' cookie
+eraseCookie('username'); // Erases the 'username' cookie
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
